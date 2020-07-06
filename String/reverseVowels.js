68247999bfe2745c33e1ca74d87a5cb1c896878d
Amelia <ganwujing@gmail.com>
2020-07-06 17:57:59 +0800

    7.6

---
+++ String/reverseVowels.js
@@ -0,0 +1,46 @@
+/**
+ * 编写一个函数，以字符串作为输入，反转该字符串中的元音字母。
+
+示例 1:
+
+输入: "hello"
+输出: "holle"
+示例 2:
+
+输入: "leetcode"
+输出: "leotcede"
+说明:
+元音字母A、E、I、O、U.不包含字母"y"。
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/reverse-vowels-of-a-string
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+ */
+var alpha = ["A", "E", "I", "O", "U"]
+var reverseVowels = function(s) {
+    var returnStr = "";
+    var sArray = s.split("")
+    var revesArray = s.split("").reverse()
+    var indexj = 0
+    for (i = 0; i < sArray.length; i++) {
+        if (alpha.includes(sArray[i])) {
+            for (j = indexj; j < revesArray.length; j++) {
+                if (alpha.includes(revesArray[j])) {
+                    indexj = j + 1
+                    returnStr = returnStr + revesArray[j]
+                    break;
+                }
+            }
+        } else {
+            returnStr = returnStr + sArray[i]
+        }
+    }
+    console.log(returnStr)
+};
+
+document.getElementById("inputStr").addEventListener("keydown", function(event) {
+    var s = document.getElementById("inputStr").value.toUpperCase()
+    if (event.keyCode == 13) {
+        reverseVowels(s);
+    }
+})
